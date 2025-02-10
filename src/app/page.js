e01102d469eb4d583f303ce1c761ebcd6e1eac36
wu11-emil-korsholm-jensen <smolf0505@gmail.com>
2025-02-10 12:09:51 +0100

    startpage styling and animation added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+import VariableButton from "@/components/VariableButton/VariableButton";
+import background from "../../public/splash-image.jpg"
+import logo from "../../public/logo.jpg"
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <h1><span>LANDRUP</span><span>DANS</span></h1>
+      <Image className="startpage-background fade-animation" src={background} height={500} width={500} alt="background image" priority/>
+      <Image className="startpage-logo fade-animation" src={logo} height={153} width={341} alt="logo image"/>
+      <Link href={"/aktiviteter"} className="startpage-button-container button-appear">
+        <VariableButton text={"Kom i gang"} className="startpage-button"/>
+      </Link>
     </>
   )
 }
--- src/components/VariableButton/VariableButton.jsx
+++ src/components/VariableButton/VariableButton.jsx
@@ -1,3 +1,11 @@
-export default function VariableButton(){
-    return null
+"use client"
+
+export default function VariableButton({ text, clickFunction, className="" }){
+    return (
+        <>
+            <button className={"variable-button " + className} onClick={clickFunction}>
+                <p>{text}</p>
+            </button>
+        </>
+    )
 }
